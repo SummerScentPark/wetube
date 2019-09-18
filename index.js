cd5d3010e7a367be4219e38bf06a15a5e7c17437
Summerscent Park <summerscentpark@gmail.com>
2019-09-18 21:27:12 +0900

    2.6 Express Core:Middleware

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("Profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
